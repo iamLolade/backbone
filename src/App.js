fdbe53712c8bc584912e00ef13b61eb1b67d1057
iamLolade <adeniyiotemade@gmail.com>
2022-02-20 14:00:42 +0100

    Created and updated README file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Article from "./components/Article";
 import User from "./components/User";
+import brand from "./image/bone.png"
 
 function App() {
   return (
     <div className="App">
       <nav>
+        <img src={brand} alt="Brand-logo" />
         <h1>Back Bone</h1>
       </nav>
       <div className="content">
